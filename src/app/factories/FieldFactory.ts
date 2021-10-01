f89ab7d8bd4ddeefa330882f3380c4565d08cb48
tobidot <Object.name@live.de>
2021-10-01 16:37:52 +0200

    Extract advacnes to class entitiy system and apply to factories

--- src/app/factories/FieldFactory.ts
+++ src/app/factories/FieldFactory.ts
@@ -1,66 +1,76 @@
+import { ClassEntityFactory } from "@game.object/ts-class-entities";
 import { Field, FieldColor, FieldType } from "../components/Field";
 import { Main } from "../Main";
 
-export class FieldFactory {
-
-    protected model: Field;
+export class FieldFactory extends ClassEntityFactory<Field> {
 
     public constructor(
-        public main: Main
+        public parent: Main
     ) {
-        this.model = new Field(main);
+        super(Field, parent);
     }
 
     public empty(): FieldFactory {
-        this.model.props.type = FieldType.EMPTY;
-        this.model.props.color = FieldColor.NONE;
-        this.model.props.gravity_radius = 0;
-        return this;
+        return this.after_create((entity: Field) => {
+            entity.props.type = FieldType.EMPTY;
+            entity.props.color = FieldColor.NONE;
+            entity.props.gravity_radius = 0;
+            return entity;
+        });
     }
 
 
     public black_hole(): FieldFactory {
-        this.model.props.type = FieldType.BLACK_HOLE;
-        this.model.props.color = FieldColor.NONE;
-        this.model.props.gravity_radius = 99;
-        return this;
+        return this.after_create((entity: Field) => {
+            entity.props.type = FieldType.BLACK_HOLE;
+            entity.props.color = FieldColor.NONE;
+            entity.props.gravity_radius = 99;
+            return entity;
+        });
     }
 
     public red_giant(): FieldFactory {
-        this.model.props.type = FieldType.RED_GIANT;
-        this.model.props.color = FieldColor.RED;
-        this.model.props.gravity_radius = 5;
-        return this;
+        return this.after_create((entity: Field) => {
+            entity.props.type = FieldType.RED_GIANT;
+            entity.props.color = FieldColor.RED;
+            entity.props.gravity_radius = 5;
+            return entity;
+        });
     }
 
     public yellow_dwarf(): FieldFactory {
-        this.model.props.type = FieldType.YELLOW_DWARF;
-        this.model.props.color = FieldColor.YELLOW;
-        this.model.props.gravity_radius = 3;
-        return this;
+        return this.after_create((entity: Field) => {
+            entity.props.type = FieldType.YELLOW_DWARF;
+            entity.props.color = FieldColor.YELLOW;
+            entity.props.gravity_radius = 3;
+            return entity;
+        });
     }
 
     public gas_giant(): FieldFactory {
-        this.model.props.type = FieldType.GAS_GIANT;
-        this.model.props.color = FieldColor.PURPLE;
-        this.model.props.gravity_radius = 0;
-        return this;
+        return this.after_create((entity: Field) => {
+            entity.props.type = FieldType.GAS_GIANT;
+            entity.props.color = FieldColor.PURPLE;
+            entity.props.gravity_radius = 0;
+            return entity;
+        });
     }
 
     public habitable_planet(): FieldFactory {
-        this.model.props.type = FieldType.HABITABLE_PLANET;
-        this.model.props.color = FieldColor.BLUE;
-        this.model.props.gravity_radius = 0;
-        return this;
+        return this.after_create((entity: Field) => {
+            entity.props.type = FieldType.HABITABLE_PLANET;
+            entity.props.color = FieldColor.BLUE;
+            entity.props.gravity_radius = 0;
+            return entity;
+        });
     }
 
     public position(x: number, y: number): FieldFactory {
-        this.model.props.position.set(x, y);
+        return this.after_create((entity: Field) => {
+            entity.props.position.set(x, y);
+            return entity;
+        });
         return this;
-    }
-
-    public create(): Field {
-        return this.model;
     }
 
 }
